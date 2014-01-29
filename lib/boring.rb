02b0c9199fa94669c84a15ce87d54e2f9d95889c
Andrew Bloom <andrew@andrewkbloom.com>
2014-01-29 01:21:31 -0600

    getting the video player loading

--- lib/boring.rb
+++ lib/boring.rb
@@ -1,15 +1,23 @@
 require "boring/version"
+require "boring/video"
 
 module Boring
+  extend self
+
   attr_accessor :videos
   self.videos = []
 
   def init(video_path)
-    video_path = File.expand_path video_path
+    video_path = File.join video_path, "*"
 
     Dir.glob(video_path).each do |path|
-      output = `file -b -i #{path}`
-      next unless output =~ /$video/
+      output = if RUBY_PLATFORM =~ /linux/
+        `file -b -i #{path}`
+      else RUBY_PLATFORM =~ /darwin/
+        `file -b -I #{path}`
+      end
+
+      next unless output =~ /^video/
 
       @videos << Boring::Video.new(path)
     end
